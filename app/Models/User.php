0acd9ff17bd9b719fa3156192e734b9bb131de49
fauzanalhfzh <zenbangga@gmail.com>
2024-12-17 18:58:21 +0700

    FEAT: adding roles and permissions

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,15 +3,18 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Panel;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
-use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Fortify\TwoFactorAuthenticatable;
 use Laravel\Jetstream\HasProfilePhoto;
 use Laravel\Sanctum\HasApiTokens;
 
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser
 {
     use HasApiTokens;
     use HasFactory;
@@ -25,11 +28,28 @@ class User extends Authenticatable
 
     const ROLE_USER = "USER";
 
+    const ROLE_DEFAULT = self::ROLE_USER;
+
     const ROLES = [
         self::ROLE_ADMIN => 'Admin',
         self::ROLE_EDITOR => 'Editor',
         self::ROLE_USER => 'User',
     ];
+
+    public function canAccessPanel(Panel $panel): bool
+    {
+        return $this->can('view-admin', User::class);
+    }
+
+    public function isAdmin()
+    {
+        return $this->role === self::ROLE_ADMIN;
+    }
+
+    public function isEditor()
+    {
+        return$this->role === self::ROLE_EDITOR;
+    }
 
     /**
      * The attributes that are mass assignable.
