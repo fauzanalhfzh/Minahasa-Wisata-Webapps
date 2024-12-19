e86cdb0997d0a8e47d2900ebeefd7af0102b6906
fauzanalhfzh <zenbangga@gmail.com>
2024-12-19 18:32:39 +0700

    Feat: adding comment manager in filament

--- app/Filament/Resources/PostResource.php
+++ app/Filament/Resources/PostResource.php
@@ -4,6 +4,7 @@ namespace App\Filament\Resources;
 
 use App\Filament\Resources\PostResource\Pages;
 use App\Filament\Resources\PostResource\RelationManagers;
+use App\Filament\Resources\PostResource\RelationManagers\CommentsRelationManager;
 use App\Models\Post;
 use App\Models\User;
 use Filament\Forms;
@@ -90,7 +91,7 @@ class PostResource extends Resource
     public static function getRelations(): array
     {
         return [
-                //
+                CommentsRelationManager::class
             ];
     }
 
