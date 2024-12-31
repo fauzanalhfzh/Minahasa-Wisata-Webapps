8e0ae503ebacf6fd7de7dedfd641a46dbc78206a
fauzanalhfzh <zenbangga@gmail.com>
2024-12-31 15:54:47 +0700

    UPDATE: Change layout

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -1,9 +1,14 @@
 <nav class="flex items-center justify-between py-3 px-6 border-b border-gray-100">
+    <!-- Logo di kiri -->
     <div id="header-left" class="flex items-center">
         <a href="{{ route('home') }}">
             <x-application-mark class="block h-9 w-auto" />
         </a>
-        <div class="top-menu ml-10">
+    </div>
+
+    <!-- Link di tengah -->
+    <div id="header-center" class="flex-1 flex justify-center">
+        <div class="top-menu">
             <div class="flex space-x-4">
                 <x-nav-link href="{{ route('home') }}" :active="request()->routeIs('home')">
                     {{ __('Home') }}
@@ -12,15 +17,17 @@
                     {{ __('Daftar Wisata') }}
                 </x-nav-link>
                 <x-nav-link href="#">
-                    {{ __('Ekraf') }}
-                </x-nav-link>
-                <x-nav-link href="#">
                     {{ __('Kinerja') }}
+                </x-nav-link>
+                <x-nav-link href="{{ route('profil') }}" :active="request()->routeIs('profil')" >
+                    {{ __('Profil') }}
                 </x-nav-link>
             </div>
         </div>
     </div>
-    <div id="header-right" class="flex items-center md:space-x-6">
+
+    <!-- Login/Register di kanan -->
+    <div id="header-right" class="flex items-center ml-auto md:space-x-6">
         @auth
             @include('layouts.partials.header-right-auth')
         @else
